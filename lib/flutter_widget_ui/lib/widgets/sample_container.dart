fe4b5349909b1e7fefac9b1d22ad6eb28009f5fb
Miftakun Niam <miftakhunniam999@gmail.com>
2025-04-15 16:59:12 +0700

    Implement SampleContainer widget with Scaffold and styled Container

--- lib/flutter_widget_ui/lib/widgets/sample_container.dart
+++ lib/flutter_widget_ui/lib/widgets/sample_container.dart
@@ -5,6 +5,29 @@ class SampleContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Belajar Container')),
+      body: Container(
+        width: double.infinity,
+        height: 300,
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          borderRadius: BorderRadius.circular(30),
+          border: Border.all(width: 3, color: Colors.red),
+        ),
+        child: const Text(
+          'ini container',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.purple,
+            fontSize: 36,
+            fontWeight: FontWeight.w500,
+            wordSpacing: 10,
+            letterSpacing: 5,
+          ),
+        ),
+      ),
+    );
   }
 }
