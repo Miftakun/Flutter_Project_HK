f70245de89a0b1bddda1045604a9b851de6611c9
Miftakun Niam <miftakhunniam999@gmail.com>
2025-05-24 17:43:48 +0700

    Remove unused import of SampleCard widget in main.dart

--- lib/flutter_widget_ui/lib/main.dart
+++ lib/flutter_widget_ui/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_widget_ui/widgets/sample_card.dart';
 import 'package:flutter_widget_ui/widgets/sample_sizedbox.dart';
 
 void main() {
