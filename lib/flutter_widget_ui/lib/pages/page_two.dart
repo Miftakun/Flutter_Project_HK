1bbbb60839b4188ce24b53ff520deda119a0aa30
Miftakun Niam <miftakhunniam999@gmail.com>
2025-05-26 09:36:58 +0700

    Update initial route in MyApp and enhance PageTwo with DateTime picker functionality

--- lib/flutter_widget_ui/lib/pages/page_two.dart
+++ lib/flutter_widget_ui/lib/pages/page_two.dart
@@ -10,18 +10,49 @@ class PageTwo extends StatefulWidget {
 }
 
 class _PageTwoState extends State<PageTwo> {
+  late DateTime selectedDate;
+  late TimeOfDay selectedTime;
+
+  void _selectTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: selectedTime,
+    );
+    if (picked != null && picked != selectedTime) {
+      setState(() {
+        selectedTime = picked;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    selectedDate = DateTime.now();
+    selectedTime = TimeOfDay.now(); // Initialize with current time
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Page Two')),
+      appBar: AppBar(title: const Text('Page Two & DateTime Picker')),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            Navigator.pop(context);
+            // _selectDate(context);
+            _selectTime(context);
           },
           child: Column(
             mainAxisSize: MainAxisSize.min,
-            children: [const Text('Back to Page One')],
+            children: [
+              //const Text('Select Dtae'),
+              //Text(selectedDate.toLocal().toString().split(' ')[0]),
+              const Text('Select Time'),
+              Text(
+                '${selectedTime.hour.toString().padLeft(2, '0')}:${selectedTime.minute.toString().padLeft(2, '0')}',
+                style: const TextStyle(fontSize: 24),
+              ),
+            ],
           ),
         ),
       ),
